d285e53c2e6d5f07366a15bd7c2e63becb137676
xXHentaiMasterXx <45514433+xXHentaiMasterXx@users.noreply.github.com>
2018-12-01 15:17:13 +0100

    Add files via upload

--- RedditDownloader.py
+++ RedditDownloader.py
@@ -1,11 +1,11 @@
 import urllib.request
 from RedditUtils import RedditFinder
 
-def download(url):
+def download(url, folder):
     s = url.split("/")
     filename = s[len(s)-1]
     print("Downloading %s as %s " % (url, filename))
-    urllib.request.urlretrieve(url, "download/" + filename)    
+    urllib.request.urlretrieve(url, folder + filename)    
     print("Done.")
 
 settings = dict()
@@ -21,5 +21,5 @@ if title:
 finder = RedditFinder(settings)
 
 for url in finder.getResults():
-    download(url)
+    download(url, "download/")
 
